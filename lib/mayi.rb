3178f798b5258426ae49a6757e7978a7dfae2408
Darwin <darwin.git@marianna.se>
2013-05-16 10:21:14 +0200

    whops, forgot the require order

--- lib/mayi.rb
+++ lib/mayi.rb
@@ -2,7 +2,11 @@
 # Add the directory containing this file to the start of the load path if it
 # isn't there already.
 $:.unshift(File.dirname(__FILE__)) unless
-    $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module MayI
+  
+end
 
 require 'mayi/access_denied_error'
 require 'mayi/methods'
